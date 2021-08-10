8982f0f428d146e43f10f28e2a483c8f726292e2
linjiuzhou <ljz@szltech.com>
2021-08-10 18:12:41 +0800

    awesome font effect

--- src/util/async.js
+++ src/util/async.js
@@ -1,5 +1,6 @@
 /* @flow */
 
+// queue是入参，fn是每次运行的异步方法，fn必须要带一个执行完后的回调，cd是所有都执行完后的回调
 export function runQueue (queue: Array<?NavigationGuard>, fn: Function, cb: Function) {
   const step = index => {
     if (index >= queue.length) {
@@ -16,3 +17,14 @@ export function runQueue (queue: Array<?NavigationGuard>, fn: Function, cb: Func
   }
   step(0)
 }
+
+// 考虑下reduce的版本
+// emm，这种异步咋搞
+export const reduceQueue = (queue: Array<?NavigationGuard>, fn: Function, cb: Function) => {
+  if (!queue) return
+  if (queue.length < 2) {
+    fn(queue[0], cb)
+  }
+
+
+} 
