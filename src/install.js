04f5825545b92047eaf9c32f4491856fa2f7d96a
UI-Mario <JZ.Livioo@gmail.com>
2021-08-10 16:25:40 +0800

    stupid me

--- src/install.js
+++ src/install.js
@@ -4,11 +4,14 @@ import Link from './components/link'
 export let _Vue
 
 export function install (Vue) {
+  // 防止重复安装
   if (install.installed && _Vue === Vue) return
+  // 但个人感觉这种属性挂载方式还不如_Vue，起码像class那样列出来
   install.installed = true
 
   _Vue = Vue
 
+  // isDefined
   const isDef = v => v !== undefined
 
   const registerInstance = (vm, callVal) => {
@@ -21,11 +24,17 @@ export function install (Vue) {
   Vue.mixin({
     beforeCreate () {
       if (isDef(this.$options.router)) {
+        // 这段代码逻辑算是看的我有点懵
+        // this指向
+        // router和route
+        // 代码逻辑先后，什么是传进来的，什么是内部做的
         this._routerRoot = this
         this._router = this.$options.router
         this._router.init(this)
         Vue.util.defineReactive(this, '_route', this._router.history.current)
       } else {
+        // 别的不说，先看代码，this._routerRoot最后被赋值的什么
+        // :rainbow:
         this._routerRoot = (this.$parent && this.$parent._routerRoot) || this
       }
       registerInstance(this, this)
--- src/util/warn.js
+++ src/util/warn.js
@@ -6,6 +6,8 @@ export function assert (condition: any, message: string) {
   }
 }
 
+// TODO:这里很好奇的是，打包以后这个process.env什么的，还能继续判断吗
+// 在dist文件夹下还是有process存在的
 export function warn (condition: any, message: string) {
   if (process.env.NODE_ENV !== 'production' && !condition) {
     typeof console !== 'undefined' && console.warn(`[vue-router] ${message}`)
